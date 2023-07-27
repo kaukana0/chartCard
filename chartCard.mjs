dc0abec1f8c50e0061634b4ff43ace386a527953
robert <kaukana0@tuta.io>
2023-07-27 11:37:03 +0200

    dotplot vertical lines

--- chartCard.mjs
+++ chartCard.mjs
@@ -257,7 +257,9 @@ class Element extends HTMLElement {
 				showLines:false,
 				tooltipFn: this.#_tooltipExtFn2,
 				labelEveryTick: true,
-				onFinished: ()=>setTimeout(()=>this.#resize(false),50)
+				onFinished: () => setTimeout(
+					()=>this.#resize(false, () => {this.drawVerticalLines()})
+				,50)
 			})
 		}
 	}
@@ -335,7 +337,7 @@ class Element extends HTMLElement {
 			const event = new Event("expanding")
 			this.dispatchEvent(event)
 		})
-		this.#resize(false)
+		this.#resize(false, () => {this.drawVerticalLines()})
 	}
 
 	contract() {
@@ -410,6 +412,91 @@ class Element extends HTMLElement {
 			}
 		}
 	}
+
+
+	// TODO: this belongs to chart really, not to chartCard
+	drawVerticalLines() {
+
+		const x = this.shadowRoot.querySelector("#verticalLines")
+		if(x) x.remove()
+
+		const [neu,eu,nat] = getDots(this.shadowRoot)
+
+		//const out = this.shadowRoot.querySelgetDotsector("#chart2  .bb-chart-lines")
+		//const out = this.shadowRoot.querySelector("#chart2 > svg > g > g.bb-chart > g.bb-chart-lines")
+		//const out = this.shadowRoot.querySelector("#chart2 > svg > g.bb-main > g.bb-chart")
+		const out = this.shadowRoot.querySelector("#chart2 > svg  g.bb-chart-lines")
+
+		
+		if( neu.length!==eu.length || neu.length!==nat.length ) {
+			if(neu.length===0 || eu.length===0 || nat.length!==0) {
+				//some of them 0 is normal
+			} else {
+				console.error("chartCard: dot plot unequal number of dots")
+			}
+			return
+		}
+
+		const g = document.createElementNS("http://www.w3.org/2000/svg","g")
+		g.setAttribute("id", "verticalLines")
+
+		const offsetX = Number(6)
+		const offsetY = Number(5)
+	
+		for(let i=0; i<neu.length; i++) {		
+			const x = Number(neu[i].getAttribute("x"))
+			if(x<=0) {continue}
+			const [ymi,yma] = getMinMax(
+				Number(neu[i].getAttribute("y")), 
+				Number(eu[i].getAttribute("y")), 
+				Number(nat[i].getAttribute("y"))
+			)
+
+			const l = document.createElementNS("http://www.w3.org/2000/svg", "line")
+			l.setAttribute("x1", x+offsetX)
+			l.setAttribute("y1", ymi+offsetY)
+			l.setAttribute("x2", x+offsetX)
+			l.setAttribute("y2", yma+offsetY)
+			l.setAttribute("stroke", "#7fffd4")
+			l.setAttribute("stroke-width", "1")
+
+			g.appendChild(l)
+		}
+
+		out.insertAdjacentElement("afterbegin", g)	// behind the dots
+
+		function getDots(root) {
+			const byCitizenship = ["Citizens-of-a-non-EU-country",  "Citizens-of-another-EU-country", "Nationals"]
+			const byBirth = ["Born-in-a-non-EU-country", "Born-in-another-EU-country", "Native-born"]
+			function sel(bla) {return `#chart2 > svg > g > g.bb-chart > g.bb-chart-lines > g.bb-chart-line.bb-target.bb-target-${bla} > g.bb-circles > use`}
+
+			const neu = root.querySelectorAll(sel(byCitizenship[0]))
+			const eu  = root.querySelectorAll(sel(byCitizenship[1]))
+			const nat = root.querySelectorAll(sel(byCitizenship[2]))
+			if(neu.length===0 && eu.length===0 && nat.length===0) {
+				return [
+					root.querySelectorAll(sel(byBirth[0])),
+					root.querySelectorAll(sel(byBirth[1])),
+					root.querySelectorAll(sel(byBirth[2]))
+				]
+			} else {
+				return [neu,eu,nat]
+			}
+
+		}
+	
+		function getMinMax(a,b,c) {
+			if(a===0) { a=b }	// avoid 0 in case of <3 points
+			if(b===0) { b=c }
+			if(c===0) { console.debug("chartCard: dot plot all values 0") }
+
+			const min = (a<b?a:b) < c ? (a<b?a:b) : c
+			const max = (a>b?a:b) > c ? (a>b?a:b) : c
+			return [min,max]
+		}
+
+	}
+
 
 	indicateLoading() {
 		this.shadowRoot.getElementById("main").classList.add("loading")
