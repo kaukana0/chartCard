a3aab69566d31429f96a3cd63ee284383945a385
robert <kaukana0@tuta.io>
2023-12-01 10:38:20 +0100

    dedupe - take 2nd element (mid color) instead of 1st (light)

--- functionalities/legendGroups.mjs
+++ functionalities/legendGroups.mjs
@@ -1,11 +1,27 @@
+/*
+bla={
+  EU:[domel1, domel2, domel3],
+  AT:[domel1, domel2, domel3],
+}
+for each [EU,AT], remove every domel after 2nd and then 1st,
+leaving only the originally 2nd
+*/
 export function dedupe(legendDOMNode) {
   const x = legendDOMNode.querySelectorAll("span.bb-legend-item")
-  const bla = new Set()
+  const bla = new Map()
   x.forEach((e)=>{
     if(bla.has(e.innerText)) {
-      e.parentElement.remove()
+      bla.get(e.innerText).push(e)
     } else {
-      bla.add(e.innerText)
+      bla.set(e.innerText,[e])
     }
   })
+
+  bla.forEach((v,k)=>{
+    v.splice(2).forEach(e=>e.parentElement.remove())
+    if(v.length>1) {
+      v[0].parentElement.remove()
+    }
+  })
+
 }
